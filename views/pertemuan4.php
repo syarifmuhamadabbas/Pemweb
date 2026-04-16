5adb7b280e5e454c777225b2709dca84b38a7067
syarifmuhamadabbas <syarifmuhamad27316@gmail.com>
2026-04-16 11:51:51 +0700

    menambahkan pertemuan4.php

---
+++ views/pertemuan4.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" align="center" width="100%">
+        <tr>
+            <th colspan="4">Data Smartphone</th>
+        </tr>
+        <tr align="center">
+            <th width="5%">No</th>
+            <th width="20%">Nama Brand</th>
+            <th width="20%">Gambar</th>
+            <th width="55%">Keterangan</th>
+        </tr">
+        <tr align="center">
+            <td rowspan="4">1</td>
+            <td rowspan="4">Apple</td>
+            <td rowspan="4"><img src="https://www.digimap.co.id/cdn/shop/files/0788-APPMYX53ID-A-1.jpg?v=1734067891&width=823" alt=""width="100%"></td>
+        </tr>
+        <tr>
+            <td>iPhone 16 Pro Max</td>
+        </tr>
+        <tr>
+            <td>Dengan desain titanium yang memukau. Kontrol Kamera.</td>
+        </tr>
+        <tr>
+            <td>Rp 20.000.000</td>
+        </tr>
+        <tr align="center">
+            <td rowspan="4">2</td>
+            <td rowspan="4">Samsung</td>
+            <td rowspan="4"><img src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/114/MTA-158938197/samsung_samsung_galaxy_s24_ultra_5g_ram_12-256gb_-_garansi_resmi_full02_eth8va06.jpg" alt=""width="100%"></td>
+        </tr>
+        <tr>
+            <td>Samsung Galaxy S24 Ultra</td>
+        </tr>
+        <tr>
+            <td>HP Premium dengan layar besar</td>
+        </tr>
+        <tr>
+            <td>Rp 18.000.000</td>
+        </tr>
+        <tr align="center">
+            <td rowspan="4">3</td>
+            <td rowspan="4">Xiaomi</td>
+            <td rowspan="4"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNnYjEF9JKZlHAe_8ldoDT42Vy88hUjfEtMA&s" alt=""width="100%"></td>
+        </tr>
+        <tr>
+            <td>Xiaomi 12</td>
+        </tr>
+        <tr>
+            <td>HP flagship dengan performa kencang.</td>
+        </tr>
+        <tr>
+            <td>Rp 8.000.000</td>
+        </tr>
+    </table>
+</body>
+</html>
